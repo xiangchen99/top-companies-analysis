9c1de30d15cf5b1ee61c780b854908697433f9ce
xiangchen99 <120740760+xiangchen99@users.noreply.github.com>
2024-05-29 11:54:26 -0400

    hi

--- web_scraping.py
+++ web_scraping.py
@@ -34,7 +34,7 @@ for row in column[1:]:
 df = pd.DataFrame(total_rows, columns=world_table_titles)
 
 #%%exporting into csv
-df.to_csv(r'C:\Users\xiang\Documents\Data Science\web_scraping\HighestUSCompanyRevenue.csv', index=False)
+#df.to_csv(r'C:\Users\xiang\Documents\Data Science\web_scraping\HighestUSCompanyRevenue.csv', index=False)
 
 #%%cleaning the dataframe
 df["Employees"] = df["Employees"].str.replace(",", '')
@@ -66,3 +66,46 @@ print(df.dtypes)
 #%% searching for high population
 #high population is when the employees are above 1 million
 high_pop_df = df[df["Employees"] > 1000000]
+
+
+#%% pytorch test
+import math
+
+dtype = torch.float
+device = torch.device("mps")
+
+# Create random input and output data
+x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+y = torch.sin(x)
+
+# Randomly initialize weights
+a = torch.randn((), device=device, dtype=dtype)
+b = torch.randn((), device=device, dtype=dtype)
+c = torch.randn((), device=device, dtype=dtype)
+d = torch.randn((), device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(2000):
+    # Forward pass: compute predicted y
+    y_pred = a + b * x + c * x ** 2 + d * x ** 3
+
+    # Compute and print loss
+    loss = (y_pred - y).pow(2).sum().item()
+    if t % 100 == 99:
+        print(t, loss)
+
+    # Backprop to compute gradients of a, b, c, d with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_a = grad_y_pred.sum()
+    grad_b = (grad_y_pred * x).sum()
+    grad_c = (grad_y_pred * x ** 2).sum()
+    grad_d = (grad_y_pred * x ** 3).sum()
+
+    # Update weights using gradient descent
+    a -= learning_rate * grad_a
+    b -= learning_rate * grad_b
+    c -= learning_rate * grad_c
+    d -= learning_rate * grad_d
+ 
+
+print(f'Result: y = {a.item()} + {b.item()} x + {c.item()} x^2 + {d.item()} x^3')
